30c3e1e786a651693b809090495cb88e0cfd8d4c
voonhong <vhong951221@gmail.com>
2020-11-24 22:27:12 +0800

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Scanner s = new Scanner(System.in);
+		//String in = s.nextLine();
+		
+
+		int i = 0;
+		do {
+			System.out.println("i is: " + i);
+			i++;
+		}while(i < 10); 
+		
+		
+	}
+
+}
