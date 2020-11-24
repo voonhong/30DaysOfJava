2216e89162d25bef9599bfe12db32e3cb5af957f
voonhong <vhong951221@gmail.com>
2020-11-24 23:15:08 +0800

    add input from file

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,21 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MySweetProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		Scanner s = new Scanner(System.in);
-		//String in = s.nextLine();
+		Scanner s = new Scanner(new File("students.txt"));
 		
-
-		int i = 0;
-		do {
-			System.out.println("i is: " + i);
-			i++;
-		}while(i < 10); 
+		List<String> students = new ArrayList<String>();
 		
+		while(s.hasNextLine()) {
+			students.add(s.nextLine());
+		}
 		
+		for(int i = 0; i < students.size(); i++) {
+			System.out.println("name " + students.get(i));
+		}
+		s.close();
 	}
 
 }
